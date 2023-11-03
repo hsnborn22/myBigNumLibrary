5729307a74f3a9a3d400043265de1f4dd6a2bd56
hsnborn22 <gauriephilomath@proton.me>
2023-11-03 18:51:53 +0100

    third commit

--- bignum.c
+++ bignum.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 // short --> 2 bytes, but we really just need 4 bits! optimize later.
 
@@ -23,6 +24,37 @@ int max(int a, int b) {
         return a;
     } else {
         return b;
+    }
+}
+
+/* Remove unnecessary zero method:
+Input: a string representation of a bigInt
+output: a string representation of the same bigInt without the unnecessary 0s. 
+*/
+
+char * removeUnnecessaryZeros(char * string, int length) {
+    int newStringLength = 0;
+    char * newString;
+    newString = calloc(1,sizeof(char));
+    if (string[0] == '0') {
+        int index;
+        for (int j = 1; j < length; j++) {
+            if (string[j] == '0') {
+                // skip
+            } else {
+                // save the index where the string of 'unnecessary' zeros end
+                index = j;
+                break;
+            }
+        }
+        for (int i = index; i < length; i++) {
+            newStringLength++;
+            newString = realloc(newString,sizeof(char)*newStringLength);
+            newString[i - index] = string[i];
+        }
+        return newString;
+    } else {
+        return string;
     }
 }
 
@@ -104,7 +136,7 @@ bigInt * initBigInt(char * numberString) {
         newDigits[j] = digits[j];
     }
     outputBigInt->digits = newDigits;
-    outputBigInt->representation = outputRepresentation;
+    outputBigInt->representation = removeUnnecessaryZeros(outputRepresentation, outputBigInt->digitCount);
     return outputBigInt;
 }
 
@@ -275,22 +307,213 @@ bigInt * sumBigInts(bigInt * number1, bigInt * number2) {
     }
 }
 
+/* Function to determine whether two bigInts are equal*/
+
+int bigIntIsEqualTo(bigInt * number1, bigInt * number2) {
+    return strcmp(number1->representation, number2->representation);
+}
+
+/* Function to determine whether a bigInt is greater than another bigInt*/
+
+int bigIntGreaterThan(bigInt * number1, bigInt * number2) {
+    if (number1->digitCount > number2->digitCount) {
+        return 1;
+    } else if (number1->digitCount < number2->digitCount) {
+        return 0;
+    } else {
+        if (number1->digits[0] == number2->digits[0]) {
+            int j = 0;
+            while (number1->digits[j] == number2->digits[j] && j < number1->digitCount) {
+                j++;
+            }
+            if (j == number1->digitCount) {
+                return 0;
+            } else {
+                if (number1->digits[j] > number2->digits[j]) {
+                    return 1;
+                } else if (number1->digits[j] < number2->digits[j]) {
+                    return 0;
+                }
+            }
+        } else if (number1->digits[0] > number2->digits[0]) {
+            return 1;
+        } else {
+            return 0;
+        }
+    }
+}
+
+/* Function to determine whether a bigInt is smaller than another bigInt*/
+
+int bigIntLessThan(bigInt * number1, bigInt * number2) {
+    if (number1->digitCount > number2->digitCount) {
+        return 0;
+    } else if (number1->digitCount < number2->digitCount) {
+        return 1;
+    } else {
+        if (number1->digits[0] == number2->digits[0]) {
+            int j = 0;
+            while (number1->digits[j] == number2->digits[j] && j < number1->digitCount) {
+                j++;
+            }
+            if (j == number1->digitCount) {
+                return 0;
+            } else {
+                if (number1->digits[j] > number2->digits[j]) {
+                    return 0;
+                } else if (number1->digits[j] < number2->digits[j]) {
+                    return 1;
+                }
+            }
+        } else if (number1->digits[0] > number2->digits[0]) {
+            return 0;
+        } else {
+            return 1;
+        }
+    }
+}
+
+/* Function to determine whether a bigInt is less than or equal to another bigInt */
+
+int bigIntLessEqThan(bigInt * number1, bigInt * number2) {
+    if (bigIntLessThan(number1,number2) || bigIntIsEqualTo(number1,number2)) {
+        return 1;
+    } else {
+        return 0;
+    }
+}
+
+/* Function to determine whether a bigInt is greater than or equal to another bigInt */
+
+int bigIntBiggerEqThan(bigInt * number1, bigInt * number2) {
+    if (bigIntGreaterThan(number1,number2) || bigIntIsEqualTo(number1,number2)) {
+        return 1;
+    } else {
+        return 0;
+    }
+}
+
+/* Iterative multiplication algorithm for big ints: 
+Note: I strongly advise against using it, since it's very slow. I included it only for
+completeness sake. 
+*/
+
+// ------  not coded yet ------
+
+bigInt * subtractBigInts(bigInt * number1, bigInt * number2) {
+    // Initialize the carry flag
+    int carryFlag = 0;
+    int negativeFlag;
+    // Initialize the bigInt we're going to return (the output)
+    bigInt * result;
+    short * resultDigits;
+    char * outputRepresentation;
+    // Reverse the digit lists for the inputs
+    short * temp1 = actualReverse(number1->digits, number1->digitCount);
+    short * temp2 = actualReverse(number2->digits, number2->digitCount);
+
+    // some if-else case checking
+    // case #1: the number of digits of the first number is the same as the second
+    if (number1->digitCount == number2->digitCount) {
+        int count = number1->digitCount;
+        // Allocate space for our resultDigits output bigInt.
+        resultDigits = calloc(1, sizeof(short) * count);
+        // Loop through the digits:
+        for (int i = 0; i < count; i++) {
+            // Place the difference of temp1[i] and temp2[i] in resultDigits[i], i.e
+            // the difference between the ith digits of the 1st and 2nd number.
+            resultDigits[i] = temp1[i] - temp2[i];
+            // Implement the carry logic:
+            // We check if the ith element of the resultDigits array is less than zero:
+            // if that is the case, we will borrow one from the next digit (on the left).
+            if (resultDigits[i] < 0) {
+                resultDigits[i] = (resultDigits[i] % 10) + 10;
+                int j = 0;
+                subtractionIterationLabel1:
+                j++;
+                temp1[i + j] -= 1;
+                temp1[i+j] = (temp1[i+j]%10) + 10;
+                goto subtractionIterationLabel1;
+                
+            }
+        }
+        // Check if the subtraction reduced the number of digits by one.
+        if (resultDigits[count - 1] == 0) {
+            carryFlag = -1;
+        }
+        // We know reverse back the result to keep it consistent.
+        short * actualDigits = actualReverse(resultDigits, number1->digitCount + carryFlag);
+        // We place the object properties that we desire
+        result->digits = actualDigits;
+        result->digitCount = number1->digitCount + carryFlag;
+        // We now calculate the string representation of our bignum.
+        // (I wanna remove this extra loop someday)
+        outputRepresentation = calloc(1, sizeof(char) * (result->digitCount));
+        for (int i = 0; i < result->digitCount; i++) {
+            outputRepresentation[i] = (char)(result->digits[i]) + 48;
+        }
+        result->representation = removeUnnecessaryZeros(outputRepresentation, result->digitCount);
+        // We now return the result of our bigInt addition.  
+        return result;
+    } else if (number1->digitCount > number2->digitCount) {
+        int count = number2->digitCount;
+        // Allocate space for our resultDigits output bigInt.
+        resultDigits = calloc(1, sizeof(short) * number1->digitCount);
+        // Loop through the digits:
+        for (int i = 0; i < count; i++) {
+            // Place the difference of temp1[i] and temp2[i] in resultDigits[i], i.e
+            // the difference between the ith digits of the 1st and 2nd number.
+            resultDigits[i] = temp1[i] - temp2[i];
+            // Implement the carry logic:
+            // We check if the ith element of the resultDigits array is less than zero:
+            // if that is the case, we will borrow one from the next digit (on the left).
+            if (resultDigits[i] < 0) {
+                resultDigits[i] = (resultDigits[i] % 10) + 10;
+                int j = 0;
+                subtractionIterationLabel2:
+                j++;
+                temp1[i + j] -= 1;
+                if (temp1[i+j] < 0) {
+                    temp1[i+j] = (temp1[i+j]%10) + 10;
+                    goto subtractionIterationLabel2;
+                }
+            }
+        }
+        for (int i = count; i < number1->digitCount; i++) {
+            resultDigits[i] = temp1[i+1];
+        }
+        // We know reverse back the result to keep it consistent.
+        short * actualDigits = actualReverse(resultDigits, number1->digitCount + carryFlag);
+        // We place the object properties that we desire
+        result->digits = actualDigits;
+        result->digitCount = number1->digitCount + carryFlag;
+        // We now calculate the string representation of our bignum.
+        // (I wanna remove this extra loop someday)
+        outputRepresentation = calloc(1, sizeof(char) * (result->digitCount));
+        for (int i = 0; i < result->digitCount; i++) {
+            outputRepresentation[i] = (char)(result->digits[i]) + 48;
+        }
+        result->representation = removeUnnecessaryZeros(outputRepresentation, result->digitCount);
+        // We now return the result of our bigInt addition.  
+        return result;
+    } else {
+        
+    }
+}
+
 int main(void) {
-    char string[] = "546";
-    char string2[] = "341";
+    char string[] = "1001";
+    char string2[] = "2000";
     bigInt * lol = initBigInt(string);
     bigInt * lol2 = initBigInt(string2);
-    bigInt * result = sumBigInts(lol,lol2);
-    short * temp1 = actualReverse(result->digits, result->digitCount);
+    bigInt * result = subtractBigInts(lol,lol2);
     for (int i = 0; i < result->digitCount; i++) {
         printf("%d \n", result->digits[i]);
     }
-    for (int i = 0; i< lol->digitCount; i++) {
-        printf("%d \n", lol->digits[i]);
-    }
-    free(lol);
-    free(lol2);
+    printf("String:\n");
+    printf("%s \n", result->representation);
+
     free(result);
-    free(temp1);
+    free(lol);
     return 0;
 }
